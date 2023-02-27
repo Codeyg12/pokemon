eeab9a692ff43faaac0af1fb09138e2cc6a3c526
Codeyg12 <codey.gallup@gmail.com>
2023-02-26 18:52:12 -0700

    creating obj with information needed

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 fetch("https://pokeapi.co/api/v2/pokemon/charmander/")
   .then((response) => response.json())
-  .then((data) => console.log(data.sprites.back_default));
+  .then((data) => {
+    let obj = {
+      name: data.name,
+      weight: data.weight,
+      imageFront: data.sprites.front_default,
+      imageBack: data.sprites.back_default,
+      height: data.height,
+      type: data.types[0].type.name
+    }
+
+    console.log(obj)
+  });
 
 //   console.log(85 * .22) //! WEIGHT CONVERSION
 
